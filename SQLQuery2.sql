660ada1653680cb8522d59391990514b9b80cce3
VivekG11 <vivekvk2903@gmail.com>
2021-08-02 16:39:22 +0530

    [Extending table to add Basic pay, Tax and NetPay]

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -10,3 +10,14 @@ alter table employee_payroll add department varchar(100) not null default 'SALES
 
 select * from employee_payroll ;
 
+alter table employee_payroll add BasicPay bigint;
+
+alter table employee_payroll add Deductions int;
+
+alter table employee_payroll add TaxablePay int;
+
+alter table employee_payroll add IncomeTax int;
+
+alter table employee_payroll add NetPay bigint;
+
+select * from employee_payroll;
